dd96b379de33b55fe34237040458b04f908d2411
Grzegorz Dziedzic <grzegorzdziedzic198@gmail.com>
2023-10-09 19:44:27 +0200

    lock master

--- src/CodeWars/exampleNr.java
+++ src/CodeWars/exampleNr.java
@@ -4,8 +4,6 @@ public class exampleNr {
     public static void main(String[] args) {
 
     }
-        int a = 5;
-        String b = "Hello";
     //Runtime ~28.05%
     //Memory ~99.02%
     public int solution_1(int[] nums) {
