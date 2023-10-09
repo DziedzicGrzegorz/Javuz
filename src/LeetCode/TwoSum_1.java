56e316cfa0e0dbd3594c49cd94a68462a6b567f8
Bondpl <rafal.majewski92@gmail.com>
2023-10-09 20:47:58 +0200

    do konfliktu

--- src/LeetCode/TwoSum_1.java
+++ src/LeetCode/TwoSum_1.java
@@ -4,8 +4,9 @@ public class TwoSum_1 {
     //runtime beats 32%
     //memory beats 66%
         public int[] solution_1(int[] nums, int target) {
-            for(int i = 0;i<nums.length;i++){
-                for(int j = i + 1;j < nums.length;j++)
+            int dlugosc = nums.length;
+            for(int i = 0;i<dlugosc;i++){
+                for(int j = i + 1;j < dlugosc;j++)
                     if(nums[i] + nums[j] == target){
                         return new int []{i,j};
                     }
