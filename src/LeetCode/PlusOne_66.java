da614d5357a6dfea79258c3b3b85487133ebd81e
Grzegorz Dziedzic <grzegorzdziedzic198@gmail.com>
2023-10-10 12:28:54 +0200

    test master

--- src/LeetCode/PlusOne_66.java
+++ src/LeetCode/PlusOne_66.java
@@ -6,6 +6,7 @@ public class PlusOne_66 {
     }
     //Runtime ~100%
     //Memory ~67%
+    //test mastera
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] < 9) {
