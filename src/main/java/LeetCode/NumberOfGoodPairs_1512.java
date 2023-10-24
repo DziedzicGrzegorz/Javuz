bd567df256ed091b65e20a5b3059f4d58c82efab
Bondpl <rafal.majewski92@gmail.com>
2023-10-24 19:21:52 +0200

    added NumberOfGoodPairs_1512

---
+++ src/main/java/LeetCode/NumberOfGoodPairs_1512.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+
+public class NumberOfGoodPairs_1512 {
+        public static int Solution_1(int[] nums) {
+            int wynik = 0;
+            for(int i = 0;i < nums.length;i++){
+                for(int j = 1;j< nums.length;j++){
+                    if(nums[i] == nums[j]&&i<j){
+                        wynik++;
+                    }
+                }
+            }
+            return wynik;
+        }
+
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1,1,3};
+        System.out.println(Solution_1(nums));
+    }
+}
