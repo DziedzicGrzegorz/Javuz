fe924ef83abf2b9d2a3353ab11140d1b4f2c7f18
Grzegorz Dziedzic <grzegorzdziedzic198@gmail.com>
2023-10-24 16:45:49 +0200

    feat(515_LeetCode): FindLargestValueinEachTreeRow_515
    Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).

---
+++ src/main/java/LeetCode/FindLargestValueInEachTreeRow_515/FindLargestValueInEachTreeRow_515.java
@@ -0,0 +1,101 @@
+package LeetCode.FindLargestValueInEachTreeRow_515;
+
+import java.util.*;
+
+public class FindLargestValueInEachTreeRow_515 {
+
+
+    public static void main(String[] args) {
+        FindLargestValueInEachTreeRow_515 test = new FindLargestValueInEachTreeRow_515();
+
+        System.out.println(test.solution_1(test.doMockTree(new LinkedList<>(Arrays.asList(1, 3, 2, 5, 3, null, 9,1,2)))));
+
+
+    }
+    //Runtime ~ 2ms Beats 83%
+    //Memory ~ 44 MB Beats 61%
+
+    public List<Integer> solution_1(TreeNode root) {
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+
+        if (root != null) {
+            queue.addLast(root);
+        }
+
+        while (!queue.isEmpty()) {
+
+            int levelSize = queue.size();
+            int maxValue = Integer.MIN_VALUE;
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.remove();
+                maxValue = Math.max(maxValue, node.val);
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            output.addLast(maxValue);
+
+        }
+        return output;
+    }
+
+    public TreeNode doMockTree(LinkedList<Integer> values) {
+        if (values == null || values.isEmpty()) return null;
+        TreeNode[] nodeArray = new TreeNode[values.size()];
+
+        for (int i = 0; i < values.size(); i++) {
+            Integer val = values.get(i);
+            nodeArray[i] = val == null ? null : new TreeNode(val);
+        }
+
+        int mid = values.size() / 2 - 1;
+        for (int i = 0; i <= mid; i++) {
+            if (2 * i + 1 < values.size() && nodeArray[i * 2 + 1] != null) {
+                assert nodeArray[i] != null;
+                nodeArray[i].left = nodeArray[i * 2 + 1];
+            }
+            if (2 * i + 2 < values.size() && nodeArray[i * 2 + 2] != null) {
+                assert nodeArray[i] != null;
+                nodeArray[i].right = nodeArray[i * 2 + 2];
+            }
+        }
+        return nodeArray[0];
+    }
+
+    public void printTree(TreeNode root){
+        if(root == null)
+            return;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            int n = queue.size();
+            for(int i=0;i<n;i++){
+                TreeNode node = queue.poll();
+                assert node != null;
+                System.out.print(" " + node.val);
+                if(node.left != null)
+                    queue.add(node.left);
+                if(node.right != null)
+                    queue.add(node.right);
+            }
+            System.out.println();
+        }
+    }
+    public void printTreeWithIndentation(TreeNode root, String indent) {
+        if (root == null)
+            return;
+
+        System.out.println(indent + root.val);
+        printTreeWithIndentation(root.left, indent + "  ");
+        printTreeWithIndentation(root.right, indent + "  ");
+    }
+
+}
